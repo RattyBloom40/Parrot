c9a4242247ef0726e2609c299b1313c469f71357
OTHSCS079 <OTHSCS079@KATYISD.ORG>
2018-09-28 07:30:31 -0500

    big goi now moves

--- Unity/Assets/Scripts/PlayerController.cs
+++ Unity/Assets/Scripts/PlayerController.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
+    public float speed; //store the player's movement speed
+    private Rigidbody2D rb2d; //reference to Rigidbody for Physics
 
 	// Use this for initialization
 	void Start () {
-		
+        //set r2bd to a reference of the rigidbody so it can be accessed later
+        rb2d = GetComponent<Rigidbody2D>();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal"); // gets player's horizontal movement
+        float moveVeritcal = Input.GetAxis("Vertical"); //gets player's vertical movement
+        Vector2 movement = new Vector2(moveHorizontal, moveVeritcal); //use the horizontal and vertical variables to create a vector with varialbe movement
+        rb2d.velocity = (movement * speed);//set the player speed to the current speed rather than adding or subtacting to eliminate acceleration and deceleration times
+    }
 }
