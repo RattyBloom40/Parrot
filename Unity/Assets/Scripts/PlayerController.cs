ff7371a56ed4668b1d0b438405f385a3ad24314e
OTHSCS079 <OTHSCS079@KATYISD.ORG>
2018-10-03 08:02:23 -0500

    Player Rotation now works very goog very ood

--- Unity/Assets/Scripts/PlayerController.cs
+++ Unity/Assets/Scripts/PlayerController.cs
@@ -9,6 +9,9 @@ public class PlayerController : MonoBehaviour {
     float mouseY;
     float playerX;
     float playerY;
+    public GameObject cursor;
+    Transform target;
+    float speed2;//speed used for transform.lookAt Method
     // Use this for initialization
     void Start () {
         //set r2bd to a reference of the rigidbody so it can be accessed later
@@ -22,12 +25,54 @@ public class PlayerController : MonoBehaviour {
         mouseY = Camera.main.ScreenToWorldPoint(new Vector2(0, Input.mousePosition.y)).y;
         playerX = transform.position.x;
         playerY = transform.position.y;
-        if (mouseX<playerX&&mouseY<playerY)//if the mouse is in quadrant one in relation to the player
-        {
-            transform.rotation = Quaternion.Euler(0,0,Mathf.Rad2Deg*(Mathf.Atan(mouseY-playerY/mouseX-playerX)));
-        }
-        Debug.Log((mouseY - playerY) + "/" + (mouseX - playerX)+", "+ Mathf.Atan(mouseY - playerY / mouseX - playerX));
-        /*else if()//if the mouse is in the second quadrant in relation to the player*/
+        /*
+         if (mouseX>playerX&&mouseY>playerY)//if the mouse is in quadrant 1 in relation to the player
+         {
+             transform.rotation = Quaternion.Euler(0,0,Mathf.Abs(Mathf.Rad2Deg*(Mathf.Atan(mouseY-playerY/mouseX-playerX))));
+         }
+         else if (mouseX < playerX && mouseY > playerY)//if the mouse is in quadrant 2 in relation to the player
+         {
+             transform.rotation = Quaternion.Euler(0, 0, 180 - Mathf.Abs(Mathf.Rad2Deg * (Mathf.Atan(mouseY - playerY / mouseX - playerX))));
+         }
+         else if (mouseX < playerX && mouseY < playerY)//if the mouse is in quadrant 3 in relation to the player
+         {
+             transform.rotation = Quaternion.Euler(0, 0, 180 + Mathf.Abs(Mathf.Rad2Deg * (Mathf.Atan(mouseY - playerY / mouseX - playerX))));
+         }
+         else if (mouseX > playerX && mouseY < playerY)//if the mouse is in quadrant 4 in relation to the player
+         {
+             transform.rotation = Quaternion.Euler(0, 0, 360 - Mathf.Abs(Mathf.Rad2Deg * (Mathf.Atan(mouseY - playerY / mouseX - playerX))));
+         }
+         else if(mouseX-playerX==0&&mouseY-playerY>0)//if the angle is exactly 90
+         {
+             transform.rotation = Quaternion.Euler(0, 0, 90);
+         }
+         else if (mouseX - playerX == 0 && mouseY - playerY < 0)//if the angle is 270
+         {
+             transform.rotation = Quaternion.Euler(0, 0, 270);
+         }
+         else if (mouseX - playerX >0 && mouseY-playerY == 0)//if the angle is 0
+         {
+             transform.rotation = Quaternion.Euler(0, 0, 0);
+         }
+         else if (mouseX - playerX < 0 && mouseY - playerY == 0)//if the angle is 180
+         {
+             transform.rotation = Quaternion.Euler(0, 0, 180);
+         }
+         Debug.Log((mouseY - playerY) + "/" + (mouseX - playerX)+", "+ transform.rotation.eulerAngles.z);*/
+
+
+        /*cursor.transform.position = new Vector3(mouseX, mouseY, 0);
+        transform.LookAt(cursor.transform.position, new Vector3(1, 0, 0));*/
+
+        float step = speed * Time.deltaTime;
+
+        Vector3 newDir = Vector3.RotateTowards(-transform.right ,new Vector3(playerX, playerY, 0) - new Vector3(mouseX, mouseY, 0), step, 0.0f);
+        
+        Debug.DrawRay(transform.position, newDir, Color.red);
+
+        transform.rotation = Quaternion.FromToRotation(Vector3.left,newDir);
+        
+        //
     }
     void FixedUpdate()
     {
