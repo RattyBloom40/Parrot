4def404b91c9c1e5f8804e3e3fa3bd518cb9fd2d
RattyBloom40 <e10benga@gmail.com>
2018-10-08 07:59:22 -0500

    Started dodge roll

--- Unity/Assets/Scripts/PlayerController.cs
+++ Unity/Assets/Scripts/PlayerController.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
+    public enum Status {
+        Normal,
+        Dodging
+    }
+
     public float speed; //store the player's movement speed
     private Rigidbody2D rb2d; //reference to Rigidbody for Physics
     float mouseX;
@@ -10,15 +15,12 @@ public class PlayerController : MonoBehaviour {
     float playerX;
     float playerY;
     Transform target;
-    float speed2;//speed used for transform.lookAt Method
-    // Use this for initialization
+
     void Start () {
         //set r2bd to a reference of the rigidbody so it can be accessed later
         rb2d = GetComponent<Rigidbody2D>();
 	}
-    void Update()
-    {
-        //Below Code Moves Player Perspective
+    void Update() {
         mouseX = Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, 0)).x;
         mouseY = Camera.main.ScreenToWorldPoint(new Vector2(0, Input.mousePosition.y)).y;
         playerX = transform.position.x;
@@ -31,9 +33,8 @@ public class PlayerController : MonoBehaviour {
 
         transform.rotation = Quaternion.FromToRotation(Vector3.left,newDir);
     }
-    void FixedUpdate()
-    {
-        Vector2 movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")); // use the horizontal and vertical axes to create a vector with varialbe movement
+    void FixedUpdate() {
+        Vector2 movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")); // use the horizontal and vertical axes to create a vector with variable movement
         rb2d.velocity = (movement * speed); // set the player speed to the current speed rather than adding or subtacting to eliminate acceleration and deceleration times
     }
 }
