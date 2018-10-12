0754ac9d8bc7de39ede8e006ce08d550c2619376
RattyBloom40 <e10benga@gmail.com>
2018-10-12 08:14:23 -0500

    Dodge Roll WIP

--- Unity/Assets/Scripts/PlayerController.cs
+++ Unity/Assets/Scripts/PlayerController.cs
@@ -7,30 +7,53 @@ public class PlayerController : MonoBehaviour {
         Normal,
         Dodging
     }
+    Status status = Status.Normal;
 
     public float speed; //store the player's movement speed
+    public float dodgeMulti = 1.4f;
+
     private Rigidbody2D rb2d; //reference to Rigidbody for Physics
     float mouseX;
     float mouseY;
     float playerX;
     float playerY;
 
+    public Vector2 dodge;
+
     void Start () {
         //set r2bd to a reference of the rigidbody so it can be accessed later
         rb2d = GetComponent<Rigidbody2D>();
 	}
+
     void Update() {
-        mouseX = Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, 0)).x;
-        mouseY = Camera.main.ScreenToWorldPoint(new Vector2(0, Input.mousePosition.y)).y;
-        playerX = transform.position.x;
-        playerY = transform.position.y;
-        float step = speed * Time.deltaTime;
-        Vector3 newDir = Vector3.RotateTowards(-transform.right ,new Vector3(playerX, playerY, 0) - new Vector3(mouseX, mouseY, 0), step, 0.0f);
-        transform.rotation = Quaternion.FromToRotation(Vector3.left,newDir);
-        //
+        switch (status) {
+            case Status.Normal:
+                mouseX = Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, 0)).x;
+                mouseY = Camera.main.ScreenToWorldPoint(new Vector2(0, Input.mousePosition.y)).y;
+                playerX = transform.position.x;
+                playerY = transform.position.y;
+                float step = speed * Time.deltaTime;
+                Vector3 newDir = Vector3.RotateTowards(-transform.right, new Vector3(playerX, playerY, 0) - new Vector3(mouseX, mouseY, 0), step, 0.0f);
+                transform.rotation = Quaternion.FromToRotation(Vector3.left, newDir);
+                if(Input.GetButtonDown("Fire2")) {
+                    dodge = rb2d.velocity.normalized;
+                }
+                break;
+            case Status.Dodging:
+
+                break;
+        }
     }
+
     void FixedUpdate() {
-        Vector2 movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")); // use the horizontal and vertical axes to create a vector with variable movement
-        rb2d.velocity = (movement * speed); // set the player speed to the current speed rather than adding or subtacting to eliminate acceleration and deceleration times
+        switch (status) {
+            case Status.Normal:
+                Vector2 movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")); // use the horizontal and vertical axes to create a vector with variable movement
+                rb2d.velocity = (movement * speed).normalized; // set the player speed to the current speed rather than adding or subtacting to eliminate acceleration and deceleration times
+                break;
+            case Status.Dodging:
+
+                break;
+        }
     }
 }
