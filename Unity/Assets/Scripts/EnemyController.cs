32a31bacb87f7a2f6e34ab47a18ae6539c26e1f8
RattyBloom40 <e10benga@gmail.com>
2018-11-02 07:46:20 -0500

    FSM structure

---
+++ Unity/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+    public enum State {
+        Patrol,
+        HuntPlayer
+    }
+
+    public State state;
+
+    void Update() {
+        switch(state) {
+            case State.Patrol:
+
+                break;
+            case State.HuntPlayer:
+
+                break;
+        }
+    }
+}
