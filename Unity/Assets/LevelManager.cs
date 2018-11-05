e6bc7a0290e31f882e4ba08c8e2a096736e5300c
Shuttlemax <42440837+Shuttlemax@users.noreply.github.com>
2018-11-05 08:01:59 -0600

    level manager

---
+++ Unity/Assets/LevelManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+    void switchLevel(int index)
+    {
+        AsyncOperation op = SceneManager.LoadSceneAsync(index);
+        StartCoroutine(waitTillSceneLoaded(op));
+    }
+
+    void switchLevel(string levelName)
+    {
+        AsyncOperation op = SceneManager.LoadSceneAsync(levelName);
+        StartCoroutine(waitTillSceneLoaded(op));
+    }
+
+    void resetCurLevel()
+    {
+        AsyncOperation op = SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex);
+        StartCoroutine(waitTillSceneLoaded(op));
+    }
+
+    IEnumerator waitTillSceneLoaded(AsyncOperation op)
+    {
+        while(!op.isDone)
+        {
+            yield return new WaitForEndOfFrame();
+            Debug.Log(op.progress / .9f);
+        }
+    }
+}
