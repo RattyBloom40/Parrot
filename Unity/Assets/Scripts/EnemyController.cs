a2a8f63c70b81f77e3490f0dabc7cd687cf73c69
BlueBinz <truebluebinz@gmail.com>
2018-11-05 08:07:57 -0600

    pathing begins

--- Unity/Assets/Scripts/EnemyController.cs
+++ Unity/Assets/Scripts/EnemyController.cs
@@ -4,8 +4,22 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour {
 
-    public Vector2[] path;
-    private int index = 0;
+    public Vector2[] path; //unique attributes
+    public float speed;
+    public Weapon weapon;
+
+    private float enemyX; //used for position
+    private float enemyY;
+    private Rigidbody2D rb2d;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        enemyX = transform.position.x;
+        enemyY = transform.position.y;
+    }
+
+    private int index = 0; //used for movement loop
 
     public enum State {
         Patrol,
@@ -18,17 +32,31 @@ public class EnemyController : MonoBehaviour {
         switch(state) {
             case State.Patrol:
                 if (PathTo(path[index])){
-                    index = index >= path.Length-1 ? 0 : ++index;
+                    index = index >= path.Length-1 ? 0 : ++index; //if reached path point, go to next path point
                 }
                 break;
             case State.HuntPlayer:
-                PathTo(PlayerController.player.transform.position);
+                PathTo(PlayerController.player.transform.position,weapon); //path towards the player with weaponRange in between
+                switch (weapon.type) {
+                    case Weapon.Type.Melee:
+                        break;
+                    case Weapon.Type.Ranged:
+                        //go pew pew
+                        break;
+                }
                 break;
         }
     }
 
     bool PathTo(Vector2 place)
     {
+
+        return true;
+    }
+
+    bool PathTo(Vector2 place, Weapon weapon)
+    {
+
         return true;
     }
 }
---
+++ Unity/Assets/Scripts/Weapon.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : ScriptableObject {
+    public string name;
+    public int magSize;
+    public float fireRate;
+    
+    public enum DamageType
+    {
+        Projectile,
+        Hitscan
+    }
+
+    public enum Type
+    {
+        Melee,
+        Ranged
+    }
+
+    public DamageType damageType;
+    public Type type;
+}
