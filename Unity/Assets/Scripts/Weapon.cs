269024d2feb09503e04b2181ec969c2dcc741e9c
BlueBinz <truebluebinz@gmail.com>
2018-11-05 08:08:57 -0600

    also weapons

--- Unity/Assets/Scripts/Weapon.cs
+++ Unity/Assets/Scripts/Weapon.cs
@@ -6,6 +6,7 @@ public class Weapon : ScriptableObject {
     public string name;
     public int magSize;
     public float fireRate;
+    public float weaponRange;
     
     public enum DamageType
     {
