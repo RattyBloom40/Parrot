d818b7c59a760a3b86c1837a2b20481800d2433c
RattyBloom40 <e10benga@gmail.com>
2018-11-29 08:10:31 -0600

    WIP

--- Unity/Assets/Scripts/Thread/NavRequest.cs
+++ Unity/Assets/Scripts/Thread/NavRequest.cs
@@ -16,7 +16,25 @@ namespace Thread {
         public NavRequest(Vector2 origin, Vector2 goal, NavMesh mesh)
         {
             NavMap map = new NavMap();
+            NavNode start = null;
+            NavNode end = null;
+            foreach(NavNode n in mesh.nodes)
+            {
+                if (start == null || Vector2.Distance(start.position, origin) > Vector2.Distance(n.position, origin))
+                    start = n;
+            }
+            foreach (NavNode n in mesh.nodes)
+            {
+                if (end == null || Vector2.Distance(end.position, goal) > Vector2.Distance(n.position, goal))
+                    end = n;
+            }
+            //  Start is closet node to origin, End is closest node to goal
+
         }
+
+        //NavNode CreateNodeOrder() {
+            
+        //}
     }
 
     class NavMap {
