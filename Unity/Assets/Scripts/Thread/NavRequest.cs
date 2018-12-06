8450e34ed755a8349f3c626a5d72eba2662c4f36
RattyBloom40 <e10benga@gmail.com>
2018-12-06 07:54:28 -0600

    finished Thread

--- Unity/Assets/Scripts/Thread/NavRequest.cs
+++ Unity/Assets/Scripts/Thread/NavRequest.cs
@@ -5,10 +5,8 @@ using UnityEngine;
 namespace Thread {
     public class NavRequest {
         bool done;
-        public bool isFinished
-        {
-            get
-            {
+        public bool isFinished {
+            get {
                 return done;
             }
         }
@@ -18,17 +16,14 @@ namespace Thread {
         NavMesh mesh;
         NavMap map;
 
-        public NavRequest(Vector2 origin, Vector2 goal, NavMesh mesh)
-        {
+        public NavRequest(Vector2 origin, Vector2 goal, NavMesh mesh) {
             start = null;
             end = null;
-            foreach(NavNode n in mesh.nodes)
-            {
+            foreach (NavNode n in mesh.nodes) {
                 if (start == null || Vector2.Distance(start.position, origin) > Vector2.Distance(n.position, origin))
                     start = n;
             }
-            foreach (NavNode n in mesh.nodes)
-            {
+            foreach (NavNode n in mesh.nodes) {
                 if (end == null || Vector2.Distance(end.position, goal) > Vector2.Distance(n.position, goal))
                     end = n;
             }
@@ -38,14 +33,44 @@ namespace Thread {
 
         Vector2 NextPos(Vector2 currentPos) {
             map = new NavMap();
-
+            BuildNavMap(start,0);
+            NavNode[] array = new NavNode[1];
+            array[0] = start;
+            return BuildPath(array)[0].position;
         }
 
-        void CreateNavMap(NavNode begin, int count) {
-            foreach(NavNode node in begin.adjacentNodes) {
+        NavNode[] BuildPath(NavNode[] current) {
+            NavNode newClosest = null;
+            foreach(NavNode node in current[0].adjacentNodes) {
+                if (newClosest == null)
+                    newClosest = node;
+                else {
+                    int x, y;
+                    map.nodeNumbers.TryGetValue(node, out x);
+                    map.nodeNumbers.TryGetValue(newClosest, out y);
+                    if (x < y)
+                        newClosest = node;
+                }
+            }
+            NavNode[] final = new NavNode[current.Length + 1];
+            final[0] = newClosest;
+            for(int x = 0;x<current.Length;x++) {
+                final[x + 1] = current[x];
+            }
+            if (newClosest.Equals(end))
+                return final;
+            else
+                return BuildPath(final);
+        }
+
+        void BuildNavMap(NavNode begin, int count) {
+            map.nodeNumbers.Add(begin, count);
+            if (begin.Equals(end))
+                return;
+            foreach (NavNode node in begin.adjacentNodes) {
                 if (map.nodeNumbers.ContainsKey(node))
                     continue;
-                
+                BuildNavMap(node, count + 1);
             }
         }
     }
